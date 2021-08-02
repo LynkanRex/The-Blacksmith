d86f73601b589d5613bb533ed1f5c0e717daaf77
Linus Å <71346418+LynkanRex@users.noreply.github.com>
2021-08-02 14:03:26 +0200

    Adds Dagger module and SellArea for Shop

---
+++ Assets/Scripts/SellArea.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SellArea : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Dagger>() != null)
+        {
+            FindObjectOfType<GameStateManager>().SellDagger();
+        }
+    }
+}
